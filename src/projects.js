bb13af8774d607dbad1cf006b41502463d5a7ded
Garam87 <115548822+Garam87@users.noreply.github.com>
2023-07-16 13:01:40 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+categories.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projectsContainer.classList.add("anim-out");
+  projects.forEach((project) => {
+    const projectTypes = project.dataset.type.split(" ");
+    if (filter === "all" || projectTypes.includes(filter)) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}
